e8481f1e37f0a57683c6d20c816008f4194dbdf8
Stefanus Buys <stefbuys21@gmail.com>
2023-12-25 11:22:27 +0200

    themes

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -3,15 +3,21 @@ mod automata;
 mod drunkard;
 mod rooms;
 mod prefab;
+mod themes;
 
 use crate::prelude::*;
 use rooms::RoomsArchitect;
 use automata::CellularAutomataArchitect;
 use drunkard::DrunkardsWalkArchitect;
 use prefab::apply_prefab;
+use themes::*;
 
 trait MapArchitect {
     fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
+}
+
+pub trait MapTheme : Sync + Send {
+    fn tile_to_render(&self, tile_type: TileType) -> FontCharType;
 }
 
 const NUM_ROOMS: usize = 20;
@@ -22,6 +28,7 @@ pub struct MapBuilder {
     pub monster_spawns: Vec<Point>,
     pub player_start: Point,
     pub amulet_start: Point,
+    pub theme: Box<dyn MapTheme>,
 }
 
 impl MapBuilder {
@@ -147,6 +154,12 @@ impl MapBuilder {
         };
         let mut mb = architect.new(rng);
         apply_prefab(&mut mb, rng);
+
+        mb.theme = match rng.range(0, 2) {
+            0 => DungeonTheme::new(),
+            _ => ForestTheme::new(),
+        };
+
         mb
     }
 
--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -4,7 +4,7 @@ use crate::prelude::*;
 #[read_component(FieldOfView)]
 #[read_component(Player)]
 
-pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Camera) {
+pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Camera, #[resource] theme: &Box<dyn MapTheme>) {
     let mut fov = <&FieldOfView>::query().filter(component::<Player>());
     let player_fov = fov.iter(ecs).next().unwrap();
 
@@ -25,14 +25,22 @@ pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Ca
                 } else {
                     DARK_GRAY
                 };
-                match map.tiles[idx] {
-                    TileType::Floor => {
-                        draw_batch.set(pt - offset, ColorPair::new(tint, BLACK), to_cp437('.'));
-                    }
-                    TileType::Wall => {
-                        draw_batch.set(pt - offset, ColorPair::new(tint, BLACK), to_cp437('#'));
-                    }
-                }
+                
+                let glyph = theme.tile_to_render(map.tiles[idx]);
+                draw_batch.set(
+                    pt - offset,
+                    ColorPair::new(tint, BLACK),
+                    glyph
+                );
+
+                //match map.tiles[idx] {
+                //    TileType::Floor => {
+                //        draw_batch.set(pt - offset, ColorPair::new(tint, BLACK), to_cp437('.'));
+                //    }
+                //    TileType::Wall => {
+                //        draw_batch.set(pt - offset, ColorPair::new(tint, BLACK), to_cp437('#'));
+                //    }
+                //}
             }
         }
     }
